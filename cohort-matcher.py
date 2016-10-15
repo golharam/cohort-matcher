95d78e01f04d08ddc6e51f9220e640e93f65a5e5
Ryan Golhar <ryan.golhar@bms.com>
2016-10-14 22:20:09 -0400

    Finished v1

--- cohort-matcher.py
+++ cohort-matcher.py
@@ -1,11 +1,12 @@
-# Name: cohort-matcher.py
 import argparse
 import ConfigParser
+import json
 import os
 import pysam
 import subprocess
 import sys
 import vcf
+from fisher import pvalue
 
 def parseArgs(argv = None):
 	# Do argv default this way, as doing it in the functional
@@ -65,6 +66,13 @@ def parseArgs(argv = None):
 	
 	parser_grp7 = parser.add_argument_group("Miscellaneous")
 	parser_grp7.add_argument("--aws-path", required=False, help="Specify path to aws cli")
+
+	parser_grp8 = parser.add_argument_group("Output")	
+	parser_grp8.add_argument("--output-dir", "-O", required=False,
+						default="./cohort-matcher-output",
+						help="Specify output directory for sample comparisons (Default: ./cohort-matcher-output)")
+	parser_grp8.add_argument("--short-output", "-so", required=False, default=False, action="store_true",
+						help="Short output format (Default: False")
 	'''
 	# Freebayes options
 	parser_grp3.add_argument("--fastfreebayes",  "-FF", required=False, default=False,
@@ -90,6 +98,8 @@ def parseArgs(argv = None):
 		print ""
 		print "Cache Dir: {}".format(args.cache_dir)
 		print "Scratch Dir: {}".format(args.scratch_dir)
+		print "Output Dir: {}".format(args.output_dir)
+		print "Short Report: {}".format(args.short_output)
 		print ""
 		print "Genotype Caller: {}".format(args.caller)
 		if args.caller == "freebayes":
@@ -146,11 +156,17 @@ def checkConfig(config):
 		if os.path.exists(reference_index) == False:
 			print "Reference 2 FASTA file (%s) is not indexed" % reference_index
 			exit(1)
-	
-	if config.chromosome_map is not None:
-		if os.path.exists(config.chromosome_map) == False:
-			print "Chromosome map (%s) could not be read" % config.chromosome_map
+		if config.chromosome_map is not None:
+			if os.path.exists(config.chromosome_map) == False:
+				print "Chromosome map (%s) could not be read" % config.chromosome_map
+				exit(1)
+		else:
+			print "Chromosome map must be supplied when using 2 references"
 			exit(1)
+	
+	if os.path.isdir(config.output_dir) == False:
+		print "Output directory (%s) does not exist" %config.output_dir
+		exit(1)
 		
 def readSamples(sampleSheetFile, verbose):
     if os.path.isfile(sampleSheetFile) == False:
@@ -387,7 +403,7 @@ def genotypeSample(sample, bamFile, reference, vcf, config):
 						print "Unable to generated BAM index"
 						exit(1)
 	else:
-		localBamFile = bamFile
+		localBamFile = os.path.abspath(bamFile)
 
 	''' Make sure BAM and reference have matching chromosomes '''
 	bam_chroms = get_chrom_names_from_BAM(localBamFile)
@@ -468,50 +484,123 @@ def genotypeSample(sample, bamFile, reference, vcf, config):
 	os.remove(intervalsFile)
 	return None
 
+def submitSample(sample, reference, vcf, config):
+	settings = { "sample": { "name": sample["name"],
+						     "bam": sample["bam"]
+						   }, 
+				"REFERENCE": reference,
+				"VCF": vcf,
+				"CACHE_DIR": config.cache_dir,
+				"SCRATCH_DIR": config.scratch_dir
+			}
+	configFile = os.path.join(config.scratch_dir, sample["name"] + ".json")
+	with open(configFile, 'w') as f:
+		json.dump(settings, f)
+	cmd = ["qsub", "-N", sample["name"], "-cwd", "-S", sys.executable, "-v", "CONFIGFILE="+configFile, __file__]
+
 def genotypeSamples(sampleSet, reference, vcf, config):
+	jobs = []
 	for sampleIndex in range(len(sampleSet)):
 		sample = sampleSet[sampleIndex]
 		tsvFile = os.path.join(config.cache_dir, sample["name"] + ".tsv")
 		if os.path.exists(tsvFile) == False:
 			if config.cluster == "local":
-				if sample["bam"].startswith("s3://"):
-					genotypeSample(sample["name"], sample["bam"], reference, vcf, config)
-				else:
-					genotypeSample(sample["name"], os.path.abspath(sample["bam"]), reference, vcf, config)
+				genotypeSample(sample["name"], sample["bam"], reference, vcf, config)
+			elif config.cluster == "sge":
+				jobId = submitSample(sample, reference, vcf, config)
+				jobs.append(jobId)
+		
+def get_chrom_data_from_map(chrom_map_file):
+    chrom_ct = 0
+    default_chroms = []
+    alternate_chroms = []
+    def_to_alt = {}
+    alt_to_def = {}
+	
+    with open(chrom_map_file, 'r') as fin:
+		header = fin.readline()
+		for line in fin:
+			if line.strip() == "":
+				continue
+			chrom_ct += 1
+			bits = line.strip().split()
+			default_chroms.append(bits[0])
+			alternate_chroms.append(bits[1])
+			def_to_alt[bits[0]] = bits[1]
+			alt_to_def[bits[1]] = bits[0]
+    return default_chroms, alternate_chroms, def_to_alt, alt_to_def
+		
+def same_gt(gt1, gt2):
+	if gt1 == gt2:
+		return True
+	else:
+		gt1_ = sorted(gt1.split("/"))
+		gt2_ = sorted(gt2.split("/"))
+		if gt1_ == gt2_:
+			return True
+		else:
+			return False
+
+def is_hom(gt):
+    gt_ = gt.split("/")
+    if gt_[0] == gt_[1]:
+        return True
+    else:
+        return False
+
+def is_subset(hom_gt, het_gt):
+    gt_hom = hom_gt.split("/")[0]
+    if gt_hom in het_gt:
+        return True
+    else:
+        return False
 
 def compareSamples(sampleSet1, sampleSet2, config):
+	if config.chromosome_map:
+		default_chroms, alternate_chroms, def_to_alt, alt_to_def = get_chrom_data_from_map(config.chromosome_map)
+	else:
+		default_chroms, alternate_chroms, def_to_alt, alt_to_def = None, None, None, None
+
+	matrix = {}
 	for sample1 in sampleSet1:
 		for sample2 in sampleSet2:
+			if config.verbose:
+				print "Comparing {} - {}".format(sample1["name"], sample2["name"])
+				
 			var_list = {}
+			
+			''' Get a list of variants that pass in sample 1 '''
 			tsv1 = os.path.join(config.cache_dir, sample1["name"] + ".tsv")
 			with open(tsv1, "r") as fin:
 				for line in fin:	
-				    if line.startswith("CHROM\t"):
-				        continue
-				    bits = line.strip("\n").split("\t")
-				    if bits[5] == "NA":
-				        continue
-				    elif int(bits[5]) < DP_THRESH:
-				        continue
-				    else:
-				        # add variants to list
-				        var_list["\t".join(bits[:2])] = 1
-			
-			# then parse second tsv file to get list of variants that passed in both bams
+					if line.startswith("CHROM\t"):
+						continue
+					bits = line.strip("\n").split("\t")
+					if bits[5] == "NA":
+						continue
+					elif int(bits[5]) < config.dp_threshold:
+						continue
+					else:
+						var_list["\t".join(bits[:2])] = 1
+						
+			''' then parse second tsv file to get list of variants that passed in both samples '''
 			tsv2 = os.path.join(config.cache_dir, sample2["name"] + ".tsv")
 			with open(tsv2, "r") as fin:
 				for line in fin:
-				    if line.startswith("CHROM\t"):
-				        continue
-				    bits = line.strip("\n").split("\t")
-				    var_ = "\t".join(bits[:2])
-				
-				    if var_ in var_list:
-				        var_list[var_] = 2
+					if line.startswith("CHROM\t"):
+						continue
+					bits = line.strip("\n").split("\t")
+					if alternate_chroms is not None:
+						var_ = alt_to_def[bits[0]] + "\t" + bits[1]
+					else:
+						var_ = "\t".join(bits[:2])
+
+					if var_ in var_list:
+						var_list[var_] = 2
 			
 			#-------------------------------------------------------------------------------
 			# write out bam1 variants
-			bam1_var = os.path.join(config.scratch_dir, sample1["name"] + ".variants") 
+			bam1_var = os.path.join(config.scratch_dir, "sample1.variants") 
 			with open(bam1_var, "w") as fout:
 				with open(tsv1, "r") as fin: 
 					for line in fin:
@@ -526,18 +615,217 @@ def compareSamples(sampleSet1, sampleSet2, config):
 			
 			#-------------------------------------------------------------------------------
 			# write out bam2 variants
-			bam2_var = os.path.join(config.scratch_dir, sample2["name"] + ".variants") 
+			bam2_var = os.path.join(config.scratch_dir, "sample2.variants") 
 			with open(bam2_var, "w") as fout:
 				with open(tsv2, "r") as fin: 
 					for line in fin:
-					    if line.startswith("CHROM\t"):
-					        continue
-					    bits = line.strip("\n").split("\t")
-					    out_line = "%s\t%s\t%s\t%s\t%s\n" % (bits[0], bits[1], bits[2], bits[3], bits[7])
-					    var_ = "\t".join(bits[:2])
-					    if var_ in var_list:
-					        if var_list[var_] == 2:
-					            fout.write(out_line)
+						if line.startswith("CHROM\t"):
+							continue
+						bits = line.strip("\n").split("\t")
+						out_line = "%s\t%s\t%s\t%s\t%s\n" % (bits[0], bits[1], bits[2], bits[3], bits[7])
+						if alternate_chroms is not None:
+							var_ = alt_to_def[bits[0]] + "\t" + bits[1]
+						else:
+							var_ = "\t".join(bits[:2])
+						if var_list.has_key(var_) and var_list[var_] == 2:
+							fout.write(out_line)
+
+			''' at this point sample1.variants and sample2.variants should have the same number of lines ie variants '''
+			''' get the genotypes from each sample '''
+			bam1_gt = {}
+			bam2_gt = {}
+			pos_list = []
+			with open(bam1_var, "r") as fin:
+				for line in fin:
+					bits = line.strip("\n").split("\t")
+					pos_ = "_".join(bits[:2])
+					geno = bits[4]
+					pos_list.append(pos_)
+					bam1_gt[pos_] = geno
+			with open(bam2_var, "r") as fin:
+				for line in fin:
+				    bits = line.strip("\n").split("\t")
+				    if alternate_chroms is not None:
+				    	pos_ = alt_to_def[bits[0]] + "_" + bits[1]
+				    else:
+				    	pos_ = "_".join(bits[:2])
+				    geno = bits[4]
+				    bam2_gt[pos_] = geno
+			
+			''' compare the genotypes '''
+			ct_common = 0
+			comm_hom_ct = 0
+			comm_het_ct = 0
+			ct_diff = 0
+			diff_hom_ct = 0
+			diff_het_ct = 0
+			diff_1sub2_ct = 0
+			diff_hom_het_ct = 0
+			diff_2sub1_ct = 0
+			diff_het_hom_ct = 0
+			for pos_ in pos_list:
+				gt1 = bam1_gt[pos_]
+				gt2 = bam2_gt[pos_]
+				# if genotypes are the same
+				if same_gt(gt1, gt2):
+					ct_common += 1
+					if is_hom(gt1):
+						comm_hom_ct += 1
+					else:
+						comm_het_ct += 1
+				else:
+					ct_diff += 1
+					# both are hom and different
+					if is_hom(gt1) and is_hom(gt2):
+						diff_hom_ct += 1
+					# both are het and different
+					elif is_hom(gt1) == False and is_hom(gt2) == False:
+						diff_het_ct += 1
+					# one is hom, one is het, test for subset
+					elif is_hom(gt1):
+						if is_subset(gt1, gt2):
+							diff_1sub2_ct += 1
+						else:
+							diff_hom_het_ct += 1
+					elif is_hom(gt2):
+						if is_subset(gt2, gt1):
+							diff_2sub1_ct += 1
+						else:
+							diff_het_hom_ct += 1
+					else:
+						print "WTF?"
+						print gt1, gt2
+			total_compared = ct_common + ct_diff
+			frac_common_plus = 0
+			frac_common = 0
+			if total_compared > 0:
+				frac_common = float(ct_common)/total_compared
+				frac_common_plus = float(ct_common + max(diff_2sub1_ct, diff_1sub2_ct))/total_compared
+			# test of allele-specific genotype subsets
+			allele_subset = ""
+			sub_sum = diff_1sub2_ct + diff_2sub1_ct
+
+			# don't bother if fewer than 10
+			if sub_sum > 10:
+				pv_set = pvalue(diff_1sub2_ct, diff_2sub1_ct, ct_diff/2, ct_diff/2)
+				pv_ = min(pv_set.left_tail, pv_set.right_tail)
+				if pv_ < 0.05:
+					if diff_1sub2_ct < diff_2sub1_ct:
+						allele_subset = "2sub1"
+						frac_common_plus = float(ct_common + diff_2sub1_ct) / total_compared
+					else:
+						allele_subset = "1sub2"
+						frac_common_plus = float(ct_common + diff_1sub2_ct) / total_compared
+			if total_compared <= 20:
+				judgement = "Inconclusive: Too few loci to compare"
+			elif total_compared <= 100:
+				# allow for 0.90 frac_common for low loci count
+				if frac_common >= 0.9 or frac_common_plus >= 0.9:
+					judgement = "LIKELY SAME SOURCE: %s" % A_BIT_LOW
+					short_judgement = "LIKELY SAME"
+					# if there is possible allele-specific genotype subset
+					if allele_subset == "1sub2" or allele_subset == "2sub1":
+						sub_ = allele_subset.split("sub")[0]
+						over_ = allele_subset.split("sub")[1]
+						judgement += """BAM%s genotype appears to be a subset of BAM%s. Possibly BAM%s is RNA-seq data or BAM%s is contaminated.""" % (sub_, over_, sub_, over_)
+						short_judgement += ". (BAM%s is subset of BAM%s)" % (sub_, over_)
+				elif frac_common <= 0.6:
+					judgement = "LIKELY FROM DIFFERENT SOURCES: %s" % A_BIT_LOW
+					short_judgement = "LIKELY DIFFERENT"
+				else:
+					judgement = "INCONCLUSIVE: %s" % A_BIT_LOW
+					short_judgement = "INCONCLUSIVE"
+			# 3. >100 sites compared
+			else:
+				if frac_common >= 0.95:
+					judgement = "BAM FILES ARE FROM THE SAME SOURCE"
+					short_judgement = "SAME"
+				elif frac_common_plus >= 0.95:
+					judgement = "BAM FILES ARE VERY LIKELY FROM THE SAME SOURCE"
+					if allele_subset == "1sub2" or allele_subset == "2sub1":
+						sub_ = allele_subset.split("sub")[0]
+						over_ = allele_subset.split("sub")[1]
+						judgement += """, but with possible allele specific genotype.\nBAM%s genotype appears to be a subset of BAM%s. Possibly BAM%s is RNA-seq data or BAM%s is contaminated. """ % (sub_, over_, sub_, over_)
+						short_judgement += ". (BAM%s is subset of BAM%s)" % (sub_, over_)
+				elif frac_common <= 0.6:
+					judgement = "BAM FILES ARE FROM DIFFERENT SOURCES"
+					short_judgement = "DIFFERENT"
+				elif frac_common >= 0.8:
+					judgement = """LIKELY FROM THE SAME SOURCE. However, the fraction of sites with common genotype is lower than expected. This can happen with samples with low coverage."""
+					short_judgement = "LIKELY SAME"
+				else:
+					judgement = "LIKELY FROM DIFFERENT SOURCES"
+					short_judgement = "LIKELY DIFFERENT"
+
+			# so pad numeric string to 6 spaces
+			diff_hom = ("%d" % diff_hom_ct).rjust(5)
+			diff_het = ("%d" % diff_het_ct).rjust(5)
+			diff_het_hom = ("%d" % diff_het_hom_ct).rjust(5)
+			diff_hom_het = ("%d" % diff_hom_het_ct).rjust(5)
+			diff_1sub2 = ("%d" % diff_1sub2_ct).rjust(5)
+			diff_2sub1 = ("%d" % diff_2sub1_ct).rjust(5)
+			std_report_str = """sample1:\t%s
+sample2:\t%s
+variants:\t%s
+depth threshold: %d
+________________________________________
+
+Positions with same genotype:   %d
+    breakdown:    hom: %d
+                  het: %d
+________________________________________
+
+Positions with diff genotype:   %d
+	 breakdown:
+                         SAMPLE 1
+                  | het  | hom  | subset
+            -------+------+------+------
+            het   |%s |%s |%s |
+            -------+------+------+------
+SAMPLE 2    hom   |%s |%s |   -  |
+            -------+------+------+------
+            subset|%s |   -  |   -  |
+________________________________________
+
+Total sites compared: %d
+Fraction of common: %f (%d/%d)
+________________________________________
+CONCLUSION:
+%s"""  % (sample1["name"], sample2["name"], config.vcf, config.dp_threshold,
+					ct_common, comm_hom_ct, comm_het_ct,
+					ct_diff, diff_het, diff_hom_het, diff_1sub2, diff_het_hom, diff_hom, diff_2sub1,
+					total_compared, frac_common, ct_common, total_compared, judgement)
+			short_report_str = """# sample1\t sample2\t DP_thresh\t FracCommon\t Same\t Same_hom\t Same_het\t Different\t 1het-2het\t 1het-2hom\t 1het-2sub\t 1hom-2het\t 1hom-2hom\t 1sub-2het\t Conclusion
+			%s\t%s\t%d\t%f\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%s""" % (sample1["name"],
+				   sample2["name"], config.dp_threshold, frac_common, ct_common, comm_hom_ct, comm_het_ct,
+				   ct_diff, diff_het_ct, diff_het_hom_ct, diff_2sub1_ct, diff_hom_het_ct,
+				   diff_hom_ct, diff_1sub2_ct, short_judgement)
+			REPORT_PATH = "%s/%s-%s" % (config.output_dir, sample1["name"], sample2["name"])
+			with open(REPORT_PATH, "w") as fout:
+				if config.short_output:
+					fout.write(short_report_str)
+					if config.verbose:
+						print short_report_str
+				else:
+					fout.write(std_report_str)
+					if config.verbose:
+						print std_report_str
+			''' save to grand matrix '''
+			if sample1["name"] not in matrix:
+				matrix[sample1["name"]] = {}
+			matrix[sample1["name"]][sample2["name"]] = frac_common
+	''' print out grand matrix '''
+	REPORT_PATH = "./cohort-matcher-results.txt"
+	with open(REPORT_PATH, "w") as fout:
+		for sample1 in sampleSet1:
+			fout.write("\t" + sample1["name"])
+		fout.write("\n")
+		for sample2 in sampleSet2:
+			fout.write(sample2["name"])
+			for sample1 in sampleSet1:
+				fout.write("\t" + matrix[sample1["name"]][sample2["name"]])
+			fout.write("\n")
+			
 def main(argv = None):
 	config = parseArgs(argv)
 	checkConfig(config)
@@ -554,4 +842,11 @@ def main(argv = None):
 	compareSamples(sampleSet1, sampleSet2, config)
 	
 if __name__ == "__main__":
-	sys.exit(main())
+	if os.environ.has_key("JOB_ID"):
+		configFile = os.environ.get("CONFIGFILE")
+		with open(configFile, 'r') as f:
+			config = json.load(f)	
+			genotypeSample(sample, bamFile, reference, vcf, config)
+			os.remove(configFile)
+	else:
+		sys.exit(main())
