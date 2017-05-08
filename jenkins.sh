101b44317316e637ef9c1b72f7a5d9c210042cae
Ryan Golhar <ryan.golhar@bms.com>
2017-05-08 16:23:20 -0400

    Re-formatting code

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#virtualenv python_env
+#source python_env/bin/activate
+#pip install -U pip setuptools coverage mock pylint
+python_env/bin/nosetests --with-coverage --cover-package cohort_matcher --cover-html
+pylint -f parseable cohort_matcher.py > pylint.out
+
+
---
+++ test_cohort_matcher.py
@@ -0,0 +1,10 @@
+import unittest
+from cohort_matcher import main
+
+class TestCohortMatcher(unittest.TestCase):
+    def test_main(self):
+        self.skipTest("not yet implemented")
+
+    def test_parseArguments(self):
+        self.skipTest("not yet implemented")
+
