1d37adb5d726b418876eb38e59237ee0a9fe44b2
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 13:46:48 -0400

    Uncomment lines in jenkins script for Jenskins

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#virtualenv python_env
-#source python_env/bin/activate
-#pip install -U pip setuptools coverage mock pylint
+virtualenv python_env
+source python_env/bin/activate
+pip install -U pip setuptools coverage mock pylint
 python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
 pylint -f parseable cohort_matcher.py > pylint.out
 
