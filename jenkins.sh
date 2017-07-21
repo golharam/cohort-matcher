93f8dcd11118a93ae29074b4ac581e8249b6eb99
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 14:45:16 -0400

    change pip install order

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ if [ ! -d python_env ]; then
   virtualenv python_env
 fi
 source python_env/bin/activate
-pip install -U setuptools nose mock pysam pyvcf fisher numpy coverage pylint
+pip install -U mock setuptools nose pysam pyvcf fisher numpy coverage pylint
 
 python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
 pylint -f parseable cohort_matcher.py > pylint.out
