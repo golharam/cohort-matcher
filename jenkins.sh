5bfb1481cdfe0fa21a72826d00d6cd738798ac48
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 14:04:12 -0400

    TEsting jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ fi
 source python_env/bin/activate
 pip install -U pip setuptools coverage mock pylint nose pysam pyvcf fisher numpy
 
-python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
-pylint -f parseable cohort_matcher.py > pylint.out
+#python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
+#pylint -f parseable cohort_matcher.py > pylint.out
 
 
