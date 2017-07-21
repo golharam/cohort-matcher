b950df3ce804a1b4eebb488ac801b808cd242c52
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 14:31:28 -0400

    Testing jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -4,9 +4,10 @@ if [ ! -d python_env ]; then
   virtualenv python_env
 fi
 source python_env/bin/activate
-pip install -U pip setuptools coverage mock pylint nose pysam pyvcf fisher numpy
+pip install -U nose mock pysam pyvcf fisher numpy coverage
+#pip install -U pip setuptools  pylint  
 
-#python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
+python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
 #pylint -f parseable cohort_matcher.py > pylint.out
 
 
