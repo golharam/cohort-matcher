47adc537effc43caf2878518d9472c2865012396
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 16:08:03 -0400

    update jenkin.sh for jenkins to run

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-if [ ! -d python_env ]; then
+f [ ! -d python_env ]; then
   virtualenv python_env
 fi
 source python_env/bin/activate
-pip install -U mock setuptools==17.1 nose pysam pyvcf fisher numpy coverage pylint
+pip install -U setuptools
+pip install -U nose mock pysam pyvcf numpy fisher
+pip install -U coverage pylint backports.functools-lru-cache singledispatch enum34 configparser
 
 python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
 pylint -f parseable cohort_matcher.py > pylint.out
-
-
