7bd31690f9cae48e744c736fe85b73388536db03
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 14:37:08 -0400

    Testing Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ if [ ! -d python_env ]; then
   virtualenv python_env
 fi
 source python_env/bin/activate
-pip install -U nose mock pysam pyvcf fisher numpy coverage
+pip install -U setuptools nose mock pysam pyvcf fisher numpy coverage
 #pip install -U pip setuptools  pylint  
 
 python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
