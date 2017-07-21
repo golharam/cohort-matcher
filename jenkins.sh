fd429d845586d1003847957b839442317567a543
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 16:18:44 -0400

    Update jenkins.sh for Jenkins to run

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 if [ ! -d python_env ]; then
   virtualenv python_env
 fi
 source python_env/bin/activate
 pip install -U setuptools
-pip install -U nose mock pysam pyvcf numpy fisher
+pip install -U nose mock pysam pyvcf numpy 
+pip install -U fisher
 pip install -U coverage pylint backports.functools-lru-cache singledispatch enum34 configparser
 
 python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
