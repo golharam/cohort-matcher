44f607c5438e9e206667a7e98c8e0290eb966578
Ryan Golhar <ryan.golhar@bms.com>
2017-07-21 14:02:00 -0400

    Updated jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-virtualenv python_env
+if [ ! -d python_env ]; then
+  virtualenv python_env
+fi
 source python_env/bin/activate
-pip install -U pip setuptools coverage mock pylint
+pip install -U pip setuptools coverage mock pylint nose pysam pyvcf fisher numpy
+
 python_env/bin/nosetests --with-coverage --cover-erase --cover-package cohort_matcher --cover-html
 pylint -f parseable cohort_matcher.py > pylint.out
 
