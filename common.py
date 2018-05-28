617ef0bf8eca374ddfb164163907cd92e7553f85
Ryan Golhar <ryan.golhar@bms.com>
2018-05-27 23:38:38 -0400

    Implementing a distributed version

---
+++ common.py
@@ -0,0 +1,108 @@
+import boto3
+import logging
+import os
+import uuid
+
+logger = logging.getLogger(__name__)
+
+def downloadFile(srcFile, destFile):
+    ''' Download file from S3 '''
+    s3 = boto3.resource('s3')
+    bucket, key = find_bucket_key(srcFile)
+    s3.meta.client.download_file(bucket, key, destFile)
+
+def uploadFile(srcFile, destFile):
+    ''' Upload a file to S3 '''
+    s3 = boto3.resource('s3')
+    bucket, key = find_bucket_key(destFile)
+    s3.meta.client.upload_file(srcFile, bucket, key,
+                               ExtraArgs={'ServerSideEncryption': 'AES256'})
+
+def find_bucket_key(s3path):
+    """
+    This is a helper function that given an s3 path such that the path is of
+    the form: bucket/key
+    It will return the bucket and the key represented by the s3 path, eg
+    if s3path == s3://bmsrd-ngs-data/P-234
+    """
+    if s3path.startswith('s3://'):
+        s3path = s3path[5:]
+    s3components = s3path.split('/')
+    bucket = s3components[0]
+    s3key = ""
+    if len(s3components) > 1:
+        s3key = '/'.join(s3components[1:])
+    return bucket, s3key
+
+def listFiles(s3_cache_dir, suffix=None):
+    ''' Return a list of files in s3_cache_dir '''
+    files = []
+    bucketName, folder = find_bucket_key(s3_cache_dir)
+    s3bucket = 's3://%s/' % bucketName
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucketName)
+    for obj in bucket.objects.filter(Prefix=folder + '/'):
+        if suffix:
+            if obj.key.endswith(suffix):
+                files.append(s3bucket + obj.key)
+        else:
+            files.append(s3bucket + obj.key)
+    return files
+
+def readSamples(sampleSheetFile):
+    '''
+    readSamples reads in a sampleSheetFile consisting of two columns:
+    name and bamfile
+    '''
+    if os.path.isfile(sampleSheetFile) is False:
+        logger.error("%s does not exist", sampleSheetFile)
+        return False
+    logger.info("Reading %s", sampleSheetFile)
+    sampleNames = []
+    samples = []
+    with open(sampleSheetFile, 'r') as f:
+        for line in f:
+            line = line.rstrip('\r\n')
+            if len(line) == 0 or line.startswith('#'):
+                continue
+
+            fields = line.split('\t')
+            if len(fields) != 2:
+                logger.error("Expect 2 fields (sampleName, bamFile) but encountered %d",
+                             len(fields))
+                return False
+
+            sampleNames.append(fields[0])
+            sample = {"name": fields[0],
+                      "bam": fields[1]}
+            samples.append(sample)
+    if len(sampleNames) != len(set(sampleNames)):
+        logger.error("Duplicate sampleids found in %s", sampleSheetFile)
+        return False
+    logger.info("Read %d samples.", len(samples))
+    return samples
+
+def generate_working_dir(working_dir_base):
+    """
+    Creates a unique working directory to combat job multitenancy
+    :param working_dir_base: base working directory
+    :return: a unique subfolder in working_dir_base with a uuid
+    """
+
+    working_dir = os.path.join(working_dir_base, str(uuid.uuid4()))
+    try:
+        os.mkdir(working_dir)
+    except Exception as e:
+        return working_dir_base
+    return working_dir
+
+def delete_working_dir(working_dir):
+    """
+    Deletes working directory
+    :param working_dir:  working directory
+    """
+
+    try:
+        shutil.rmtree(working_dir)
+    except Exception as e:
+        print ('Can\'t delete %s' % working_dir)
---
+++ compareGenotypes.py
@@ -0,0 +1,367 @@
+#!/usr/bin/env python
+'''
+Script to  compare genotypes of samples
+'''
+import argparse
+from fisher import pvalue
+import logging
+import os
+import vcf
+import sys
+
+from common import downloadFile, find_bucket_key, listFiles, readSamples, generate_working_dir, delete_working_dir
+
+__appname__ = 'compareGenotypes'
+__version__ = "0.1-alpha"
+
+logger = logging.getLogger(__appname__)
+
+def is_hom(gt):
+    ''' Test for homozygosity '''
+    gt_ = gt.split("/")
+    return gt_[0] == gt_[1]
+
+def is_same_gt(gt1, gt2):
+    ''' Check is same genotype '''
+    if gt1 == gt2:
+        return True
+    gt1_ = sorted(gt1.split("/"))
+    gt2_ = sorted(gt2.split("/"))
+    return gt1_ == gt2_
+
+def is_subset(hom_gt, het_gt):
+    ''' Test if hom_gt is subset of het_gt '''
+    gt_hom = hom_gt.split("/")[0]
+    return gt_hom in het_gt
+
+def compareGenotypes(var_list, var_list2, intersection):
+    ''' Compare genotypes of two samples '''
+    ct_common = 0
+    comm_hom_ct = 0
+    comm_het_ct = 0
+    ct_diff = 0
+    diff_hom_ct = 0
+    diff_het_ct = 0
+    diff_1sub2_ct = 0
+    diff_hom_het_ct = 0
+    diff_2sub1_ct = 0
+    diff_het_hom_ct = 0
+    for pos_ in intersection:
+        gt1 = var_list[pos_]['GT']
+        #if alternate_chroms is not None:
+        #    bits = pos_.split('\t')
+        #    gt2 = var_list2[def_to_alt[bits[0]] + "\t" + bits[1]]['GT']
+            #gt2 = def_to_alt[pos_]
+        #else:
+        gt2 = var_list2[pos_]['GT']
+
+        # if genotypes are the same
+        if is_same_gt(gt1, gt2):
+            ct_common += 1
+            if is_hom(gt1):
+                comm_hom_ct += 1
+            else:
+                comm_het_ct += 1
+        else:
+            ct_diff += 1
+            # both are hom and different
+            if is_hom(gt1) and is_hom(gt2):
+                diff_hom_ct += 1
+            # both are het and different
+            elif is_hom(gt1) is False and is_hom(gt2) is False:
+                diff_het_ct += 1
+            # one is hom, one is het, test for subset
+            elif is_hom(gt1):
+                if is_subset(gt1, gt2):
+                    diff_1sub2_ct += 1
+                else:
+                    diff_hom_het_ct += 1
+            elif is_hom(gt2):
+                if is_subset(gt2, gt1):
+                    diff_2sub1_ct += 1
+                else:
+                    diff_het_hom_ct += 1
+            else:
+                print "WTF?"
+                print gt1, gt2
+                exit(1)
+
+    total_compared = ct_common + ct_diff
+    frac_common_plus = 0
+    frac_common = 0
+    if total_compared > 0:
+        frac_common = float(ct_common)/total_compared
+        frac_common_plus = float(ct_common + max(diff_2sub1_ct,
+                                                 diff_1sub2_ct))/total_compared
+
+    # test of allele-specific genotype subsets
+    allele_subset = ""
+    sub_sum = diff_1sub2_ct + diff_2sub1_ct
+    # don't bother if fewer than 10
+    if sub_sum > 10:
+        pv_set = pvalue(diff_1sub2_ct, diff_2sub1_ct, ct_diff/2, ct_diff/2)
+        pv_ = min(pv_set.left_tail, pv_set.right_tail)
+        if pv_ < 0.05:
+            if diff_1sub2_ct < diff_2sub1_ct:
+                allele_subset = "2sub1"
+                frac_common_plus = float(ct_common + diff_2sub1_ct) / total_compared
+            else:
+                allele_subset = "1sub2"
+                frac_common_plus = float(ct_common + diff_1sub2_ct) / total_compared
+
+    results = {}
+    results['total_compared'] = total_compared
+    results['ct_common'] = ct_common
+    results['frac_common'] = frac_common
+    results['frac_common_plus'] = frac_common_plus
+    results['comm_hom_ct'] = comm_hom_ct
+    results['comm_het_ct'] = comm_het_ct
+    results['ct_diff'] = ct_diff
+    results['diff_hom_ct'] = diff_hom_ct
+    results['diff_het_ct'] = diff_het_ct
+    results['diff_hom_het_ct'] = diff_hom_het_ct
+    results['diff_het_hom_ct'] = diff_het_hom_ct
+    results['diff_1sub2_ct'] = diff_1sub2_ct
+    results['diff_2sub1_ct'] = diff_2sub1_ct
+    results['allele_subset'] = allele_subset
+    results['judgement'], results['short_judgement'] = makeJudgement(total_compared,
+                                                                     frac_common,
+                                                                     frac_common_plus,
+                                                                     allele_subset)
+    return results
+
+def makeJudgement(total_compared, frac_common, frac_common_plus, allele_subset):
+    ''' Make judgement of sample similarity based on genotype comparison '''
+    A_BIT_LOW = "the number of comparable genomic loci is a bit low. Try using a different variants list (--VCF) file which have more appropriate genomic positions for comparison."
+
+    if total_compared <= 20:
+        judgement = "Inconclusive: Too few loci to compare"
+        short_judgement = "INCONCLUSIVE"
+    elif total_compared <= 100:
+        # allow for 0.90 frac_common for low loci count
+        if frac_common >= 0.9 or frac_common_plus >= 0.9:
+            judgement = "LIKELY SAME SOURCE: %s" % A_BIT_LOW
+            short_judgement = "LIKELY SAME"
+            # if there is possible allele-specific genotype subset
+            if allele_subset == "1sub2" or allele_subset == "2sub1":
+                sub_ = allele_subset.split("sub")[0]
+                over_ = allele_subset.split("sub")[1]
+                judgement += """ BAM%s genotype appears to be a subset of BAM%s. Possibly BAM%s is RNA-seq data or BAM%s is contaminated.""" % (sub_, over_, sub_, over_)
+                short_judgement += ". (BAM%s is subset of BAM%s)" % (sub_, over_)
+        elif frac_common <= 0.6:
+            judgement = "LIKELY FROM DIFFERENT SOURCES: %s" % A_BIT_LOW
+            short_judgement = "LIKELY DIFFERENT"
+        else:
+            judgement = "INCONCLUSIVE: %s" % A_BIT_LOW
+            short_judgement = "INCONCLUSIVE"
+    # 3. >100 sites compared
+    else:
+        if frac_common >= 0.95:
+            judgement = "BAM FILES ARE FROM THE SAME SOURCE"
+            short_judgement = "SAME"
+        elif frac_common_plus >= 0.95:
+            judgement = "BAM FILES ARE VERY LIKELY FROM THE SAME SOURCE"
+            short_judgement = "LIKELY SAME"
+            if allele_subset == "1sub2" or allele_subset == "2sub1":
+                sub_ = allele_subset.split("sub")[0]
+                over_ = allele_subset.split("sub")[1]
+                judgement += ", but with possible allele specific genotype. BAM%s genotype appears to be a subset of BAM%s. Possibly BAM%s is RNA-seq data or BAM%s is contaminated." % (sub_, over_, sub_, over_)
+                short_judgement += ". (BAM%s is subset of BAM%s)" % (sub_, over_)
+        elif frac_common <= 0.6:
+            judgement = "BAM FILES ARE FROM DIFFERENT SOURCES"
+            short_judgement = "DIFFERENT"
+        elif frac_common >= 0.8:
+            judgement = "LIKELY FROM THE SAME SOURCE. However, the fraction of sites with common genotype is lower than expected. This can happen with samples with low coverage."
+            short_judgement = "LIKELY SAME"
+        else:
+            judgement = "LIKELY FROM DIFFERENT SOURCES"
+            short_judgement = "LIKELY DIFFERENT"
+
+    return judgement, short_judgement
+
+def getIntersectingVariants(var_list, var_list2):
+    ''' Given two tsv list of variants, find the intersection '''
+    intersection = []
+    for var2 in var_list2:
+        var_ = var2
+        if var_ in var_list:
+            intersection.append(var_)
+    return intersection
+
+def main(argv):
+    ''' Main Entry Point '''
+    args = parseArguments(argv)
+    logging.basicConfig(level=args.log_level)
+    logger.info("%s v%s" % (__appname__, __version__))
+    logger.info(args)
+
+    working_dir = generate_working_dir(args.working_dir)
+    logger.info("Working in %s", working_dir)
+
+    sampleName = args.sample
+    s3_vcfFile = "%s/%s.vcf" % (args.s3_cache_dir, sampleName)
+    vcfFile = "%s/%s.vcf" % (working_dir, sampleName)
+    downloadFile(s3_vcfFile, vcfFile)
+    tsvFile = "%s/%s.tsv" % (working_dir, sampleName)
+    VCFtoTSV(vcfFile, tsvFile)
+    var_list = get_tsv_variants(tsvFile, args.dp_threshold)
+    os.remove(vcfFile)
+    os.remove(tsvFile)
+
+    meltedResultsFile = "%s/%s.meltedResults.txt" % (working_dir, sampleName)
+    fout = open(meltedResultsFile, "w")
+    fout.write("Sample1\tSample2\tn_S1\tn_S2\tSNPs_Compared\tFraction_Match\tJudgement\n")
+
+    if args.bamsheet.startswith('s3://'):
+        bamsheet = "%s/bamsheet.txt" % working_dir
+        downloadFile(args.bamsheet, bamsheet)
+    else:
+        bamsheet = args.bamsheet
+    samples = readSamples(bamsheet)
+    for sample in samples:
+        if sample['name'] == sampleName:
+            continue
+        logger.info("Comparing %s - %s", sampleName, sample['name'])
+        
+        s3_vcfFile = "%s/%s.vcf" % (args.s3_cache_dir, sample['name'])
+        vcfFile2 = "%s/%s.vcf" % (working_dir, sample['name'])
+        downloadFile(s3_vcfFile, vcfFile2)
+        tsvFile2 = "%s/%s.tsv" % (working_dir, sampleName)
+        VCFtoTSV(vcfFile2, tsvFile2)
+        var_list2 = get_tsv_variants(tsvFile2, args.dp_threshold)
+        os.remove(vcfFile2)
+        os.remove(tsvFile2)
+
+        # compare the genotypes
+        intersection = getIntersectingVariants(var_list, var_list2)
+        results = compareGenotypes(var_list, var_list2, intersection)
+        logger.info("\t%.4f / %d - %s", results['frac_common'], results['total_compared'],
+                    results['short_judgement'])
+
+        n1 = '%d' % len(var_list)
+        n2 = '%d' % len(var_list2)
+        fm = '%.4f' % results['frac_common']
+        tc = '%d' % results['total_compared']
+        j = results['short_judgement']
+        fout.write(sampleName + "\t" + sample["name"] + "\t" + 
+                   n1 + "\t" + n2 + "\t" + 
+                   tc + "\t" + fm + "\t" + j + "\n")
+    fout.close()
+    uploadFile(meltedResultsFile, "%s/%s" % (args.s3_cache_dir, os.path.basename(meltedResultsFile)))
+
+    logger.info('Cleaning up working dir')
+    delete_working_dir(working_dir)
+    logger.info('Completed')
+
+def parseArguments(argv):
+    ''' Parse arguments '''
+    parser = argparse.ArgumentParser(description='Compare a sample to a set of samples')
+    parser.add_argument('--log-level', help="Prints warnings to console by default",
+                        default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR"])
+
+    parser.add_argument('-b', '--bamsheet', required=True, help="Bamsheet of all samples")
+    parser.add_argument('-s', '--sample', required=True, help="Sample of interest")
+    parser.add_argument("--s3-cache-dir", "-CD", required=True, 
+                        help="Specify S3 path for cached VCF/TSV files")
+    parser.add_argument('--working_dir', type=str, default='/scratch')
+
+    parser.add_argument("-t", "--dp_threshold", default=15, help="Depth of Coverage Threshold")
+
+    args = parser.parse_args(argv)
+    return args
+
+def get_tsv_variants(tsvFile, dp_threshold):
+    ''' Return a list of variants that pass threshold '''
+    var_list = {}
+    with open(tsvFile, "r") as fin:
+        for line in fin:
+            if line.startswith("CHROM\t"):
+                continue
+            bits = line.strip("\n").split("\t")
+            if bits[5] == "NA":
+                continue
+            elif int(bits[5]) < dp_threshold:
+                continue
+            else:
+                var_list["\t".join(bits[:2])] = {'REF': bits[2], 'ALT': bits[3],
+                                                 'DP': int(bits[5]), 'GT': bits[7]}
+    return var_list
+
+def VCFtoTSV(invcf, outtsv, caller="freebayes"):
+    '''
+    Convert a VCF to TSV
+    '''
+    fout = open(outtsv, "w")
+    vcf_in = vcf.Reader(open(invcf, "r"))
+    var_ct = 0
+    if caller == "gatk" or caller == "varscan":
+        fields_to_extract = ["CHROM", "POS", "REF", "ALT", "QUAL", "DP", "AD", "GT"]
+    elif caller == "freebayes":
+        fields_to_extract = ["CHROM", "POS", "REF", "ALT", "QUAL", "DP", "AO", "GT"]
+    fout.write("%s\n" % "\t".join(fields_to_extract))
+    for var in vcf_in:
+        chrom_ = var.CHROM.replace("chr", "")
+        pos_ = str(var.POS)
+        ref_ = var.REF
+        qual_ = str(var.QUAL)
+        if caller == "varscan":
+            qual_ = str(var.samples[0]["GQ"])
+
+        dp_ = "0"
+        #ad_or_ao = "NA"
+        ad_str = "NA"
+        gt_ = "NA"
+        alt_str = ""
+
+        if var.samples[0].called is False:
+            continue
+
+        # usually need to bypass indels, however,
+        # homozygous REF is considered indel by pyvcf... WTF?
+        if var.is_monomorphic:
+            alt_str = "."
+            gt_ = "%s/%s" % (ref_, ref_)
+            if caller == "freebayes":
+                dp_ = var.samples[0].data.DP
+                ro_ = var.samples[0].data.RO
+                ad_str = str(dp_ - ro_)
+            elif caller == "gatk":
+                dp_ = var.samples[0].data.DP
+                ad_str = "0"
+            elif caller == "varscan":
+                dp_ = var.INFO["ADP"]
+            dp_ = str(dp_)
+        else:
+            alt_ = var.ALT[0]
+            alt_str = "."
+            if alt_ != None:
+                alt_str = alt_.sequence
+            if caller == "freebayes" or caller == "gatk":
+                dp_ = str(var.INFO["DP"])
+            else:
+                dp_ = str(var.INFO["ADP"])
+
+            gt_ = var.samples[0].gt_bases
+
+            if caller == "gatk":
+                ad_ = var.samples[0]["AD"]
+                for a_ in ad_:
+                    ad_str += ",%d" % a_
+                ad_str = ad_str[1:]
+            elif caller == "varscan":
+                ad_str = str(var.samples[0]["AD"])
+            else:
+                ad_str = str(var.samples[0]["AO"])
+
+        if ad_str == "NA":
+            ad_str = "0"
+
+        data_bits = [chrom_, pos_, ref_, alt_str, qual_, dp_, ad_str, gt_]
+        fout.write("%s\n" % "\t".join(data_bits))
+        var_ct += 1
+    fout.close()
+    return var_ct
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
