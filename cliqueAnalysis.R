bb0885311c1119e666300f459045a2f603009197
Ryan Golhar <ryan.golhar@bms.com>
2018-10-09 21:06:31 +0000

    Code clean up

--- cliqueAnalysis.R
+++ cliqueAnalysis.R
@@ -1,44 +1,56 @@
-install.packages("igraph")
+#install.packages("igraph")
 library(igraph)
 library(reshape2)
 library(plyr)
-#Clean
-rm(list = ls())
-snpt<-150
-snpot<-0.7
-#Read the edges that have the SNPs overlap and SNP num
-cm<-read.table("meltedResults.txt",sep="\t",head=TRUE)
-cm$weighted<-cm$Fraction_Match*log(cm$SNPs_Compared)
-#get an idea about the set
-cm.sorted<-cm[with(cm,order(Fraction_Match)),]
-cm.sorted$color<-'black'
-cm.sorted$color[cm.sorted$SNPs_Compared<100]<-'red'
-cm.sorted$color[cm.sorted$SNPs_Compared<150&cm.sorted$SNPs_Compared>99]<-'orange'
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  cohort_matcher_results <- "meltedResults.txt"
+} else {
+  cohort_matcher_results <- args[1]
+}
+paste("Reading", cohort_matcher_results, sep=" ")
+cm <- read.table(cohort_matcher_results, header=TRUE, sep="\t")
+
+# Set the parameters
+snp_threshold <- 150
+snp_cutoff <- 0.7
+paste("Using SNP Threshold:", snp_threshold, sep=" ")
+paste("Using SNP Cut-off:", snp_cutoff, sep=" ")
+
+# Read the edges that have the SNPs overlap and SNP num
+cm$weighted <- cm$Fraction_Match * log(cm$SNPs_Compared)
+
+# Get an idea about the set
+cm.sorted <- cm[with(cm,order(Fraction_Match)),]
+cm.sorted$color <- 'black'
+cm.sorted$color[cm.sorted$SNPs_Compared<100] <- 'red'
+cm.sorted$color[cm.sorted$SNPs_Compared<150&cm.sorted$SNPs_Compared>99] <- 'orange'
 png("snpdistr.png")
-plot(cm.sorted$Fraction_Match,col=cm.sorted$color,main="SNP
-overlap, red is <100 SNPs and orange <150 SNPs")
+plot(cm.sorted$Fraction_Match,col=cm.sorted$color,main="SNP overlap, red is <100 SNPs and orange <150 SNPs")
 abline(0.7,0,col="red")
 abline(0.6,0,col="orange")
 dev.off()
+
 png("weightDistr.png")
-cm.sorted<-cm[with(cm,order(weighted)),]
-cm.sorted$color<-'black'
-cm.sorted$color[cm.sorted$SNPs_Compared<100]<-'red'
-cm.sorted$color[cm.sorted$SNPs_Compared<150&cm.sorted$SNPs_Compared>99]<-'orange'
-plot(cm.sorted$weighted,col=cm.sorted$color,main="SNP
-overlap, red is <100 SNPs and orange <150 SNPs")
+cm.sorted <- cm[with(cm,order(weighted)),]
+cm.sorted$color <- 'black'
+cm.sorted$color[cm.sorted$SNPs_Compared<100] <- 'red'
+cm.sorted$color[cm.sorted$SNPs_Compared<150&cm.sorted$SNPs_Compared>99] <- 'orange'
+plot(cm.sorted$weighted,col=cm.sorted$color,main="SNP overlap, red is <100 SNPs and orange <150 SNPs")
 abline(0.7,0,col="red")
 abline(0.6,0,col="orange")
 dev.off()
-#Filter, should be args?
-#cm.graph<-cm[cm$Fraction_Match>snpot&cm$SNPs_Compared>snpt,c('Sample1','Sample2')]
-cm.graph<-cm[cm$weighted>4,c('Sample1','Sample2')]
-#Get the graph
-cm.g<-graph_from_edgelist(as.matrix(cm.graph),directed=FALSE)
-#Look only at the cliques with size >2 and do not report
-sub-cliques
-cm.maxcliques<-max_cliques(cm.g,min=2)
-#Kludgy, but works
+
+# Filter, should be args?
+#cm.graph<-cm[cm$Fraction_Match>snp_cutoff & cm$SNPs_Compared>snp_threshold,c('Sample1','Sample2')]
+cm.graph <- cm[cm$weighted>4,c('Sample1','Sample2')]
+
+# Get the graph
+cm.g <- graph_from_edgelist(as.matrix(cm.graph),directed=FALSE)
+# Look only at the cliques with size >2 and do not report sub-cliques
+cm.maxcliques <- max_cliques(cm.g,min=2)
+# Kludgy, but works
 capture.output(cm.maxcliques,file="maxcliques.txt")
 
 #Define how to get patients per clique
