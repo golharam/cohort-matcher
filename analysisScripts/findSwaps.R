26dc70ac599e1a90f68a469dbcde90443c4edfac
Ryan Golhar <ryan.golhar@bms.com>
2018-10-29 21:03:37 -0400

    Output results to text file and rename patient to subject

--- analysisScripts/findSwaps.R
+++ analysisScripts/findSwaps.R
@@ -4,34 +4,38 @@
 ### This script finds sample swaps based on genotyping from cohort-matcher. 
 ### Inputs:
 ### 1.  Cohort-matcher melted results
-### 2.  Sample to Patient (This should be a master list of all samples mapping to patient)
+### 2.  Sample to Subject (This should be a master list of all samples mapping to subject)
 ###
 
 ### Get the command line arguments
 library(argparser, quietly=TRUE)
 p <- arg_parser("findSampleSwaps")
 p <- add_argument(p, "--meltedResults", help="Cohort-matcher melted results", default="meltedResults.txt")
-p <- add_argument(p, "--sampleToPatient", help="Sample to patient mapping", default="sampleToPatient.txt")
+p <- add_argument(p, "--sampleToSubject", help="Sample to subject mapping", default="sampleToSubject.txt")
 p <- add_argument(p, "--showSampleLabels", help="Show sample labels in circos plot (Default: False)", flag=TRUE)
-p <- add_argument(p, "--showPatientLabels", help="Show patient labels in circos plot (Default: False)", flag=TRUE)
+p <- add_argument(p, "--showSubjectLabels", help="Show subject labels in circos plot (Default: False)", flag=TRUE)
+p <- add_argument(p, "--outputFile", help="Output file to list swaps", default="swaps.txt")
 argv <- parse_args(p)
 ###
+
+# Redirect output to file
+sink(argv$outputFile)
 
 # Read cohort matcher results
 print(paste("Reading", argv$meltedResults, sep=" "))
 cm <- read.table(argv$meltedResults, header=TRUE, sep="\t", stringsAsFactors=FALSE)
 cm.same <- cm[ which(cm$Judgement=="SAME"), ]
 
-# Read sample to Patient map
-print(paste("Reading", argv$sampleToPatient, sep=" "))
-sample_to_patient <- read.table(argv$sampleToPatient, header=FALSE, sep="\t", stringsAsFactors=FALSE, 
+# Read sample to Subject map
+print(paste("Reading", argv$sampleToSubject, sep=" "))
+sample_to_subject <- read.table(argv$sampleToSubject, header=FALSE, sep="\t", stringsAsFactors=FALSE,
                                 na.strings = "")
-colnames(sample_to_patient) <- c("sample", "patient")
+colnames(sample_to_subject) <- c("sample", "subject")
 
-# TODO: Verify the list of samples in meltedResults matches the samples in sampleToPatient
+# TODO: Verify the list of samples in meltedResults matches the samples in sampleToSubject
 
-print( paste("Read", length(unique(sample_to_patient$patient)), "patients and", 
-             length(unique(sample_to_patient$sample)), "samples", sep=" ") )
+print( paste("Read", length(unique(sample_to_subject$subject)), "subjects and",
+             length(unique(sample_to_subject$sample)), "samples", sep=" ") )
 
 # Plot the comparison results
 # TODO: Since we aren't loading a matrix anymore this code doesn't work.   I need to find a way to
@@ -44,14 +48,14 @@ print( paste("Read", length(unique(sample_to_patient$patient)), "patients and",
 
 
 # For each sample in the list of samples, get the best match
-matches <- data.frame(from_sample=character(), from_patient=character(),
-                      to_sample=character(), to_patient=character(), 
+matches <- data.frame(from_sample=character(), from_subject=character(),
+                      to_sample=character(), to_subject=character(),
                       stringsAsFactors = FALSE)
-for (sample in sample_to_patient$sample) {
+for (sample in sample_to_subject$sample) {
   print(sample)
   sample_matches <- cm[ which((cm$Sample1==sample | cm$Sample2==sample) & cm$Judgement=="SAME"), ]
-  # if sample_matches is Empty, then sample doesn't match to another patient
-  # for all sample_matches, make sure the sample matches to the same patient
+  # if sample_matches is Empty, then sample doesn't match to another subject
+  # for all sample_matches, make sure the sample matches to the same subject
   if (nrow(sample_matches) > 0) {
     print(paste("     Found", nrow(sample_matches), "match(s)", sep=" "))
     for (row_index in 1:nrow(sample_matches)) {
@@ -63,39 +67,41 @@ for (sample in sample_to_patient$sample) {
       print(paste("     Matched sample is", matched_sample, sep=" "))
       # sample is the sample of interest
       # matched_sample is the matched sample
-      # Now, make sure they are from the same patient
-      patient1 <- sample_to_patient[sample_to_patient$sample==sample, 'patient']
-      patient2 <- sample_to_patient[sample_to_patient$sample==matched_sample, 'patient']
-      print(paste("     ", patient1, "-", patient2, sep=" "))
-      if (patient1 != patient2) {
-        print(paste("     Sample ", sample, " (USUBJID: ", patient1, ") matches to ", matched_sample, 
-                    " (USUBJID: ", patient2, ")", sep=""))
+      # Now, make sure they are from the same subject
+      subject1 <- sample_to_subject[sample_to_subject$sample==sample, 'subject']
+      subject2 <- sample_to_subject[sample_to_subject$sample==matched_sample, 'subject']
+      print(paste("     ", subject1, "-", subject2, sep=" "))
+      if (subject1 != subject2) {
+        print(paste("     Sample ", sample, " (USUBJID: ", subject1, ") matches to ", matched_sample,
+                    " (USUBJID: ", subject2, ")", sep=""))
       }
-      matches <- rbind(matches, data.frame(from_sample=sample, from_patient=patient1, to_sample=matched_sample, to_patient=patient2))
+      matches <- rbind(matches, data.frame(from_sample=sample, from_subject=subject1, to_sample=matched_sample, to_subject=subject2))
     }
     
   }
 }
+sink()
+
 # Fix the data.frame to remove levels
 matches <- data.frame(lapply(matches, as.character), stringsAsFactors = FALSE)
 
 # Construct the circos plot
-patient = c(structure(matches$from_patient, names=matches$from_sample), structure(matches$to_patient, names=matches$to_sample))
-patient = patient[!duplicated(names(patient))]
-patient = patient[order(patient, names(patient))]
+subject = c(structure(matches$from_subject, names=matches$from_sample), structure(matches$to_subject, names=matches$to_sample))
+subject = subject[!duplicated(names(subject))]
+subject = subject[order(subject, names(subject))]
 
-patient_color = structure(2:(length(unique(patient))+1), names=unique(patient))
-sample_color = structure(2:(length(names(patient))+1), names=names(patient))
+subject_color = structure(2:(length(unique(subject))+1), names=unique(subject))
+sample_color = structure(2:(length(names(subject))+1), names=names(subject))
 
-# If we don't have a massive # of samples (not sure what massive is yet), this code works
 library(circlize)
-gap.after = do.call("c", lapply(table(patient), function(i) c(rep(2, i-1), (length(names(patient))+1))))
-circos.par(gap.after = gap.after, cell.padding = c(0, 0, 0, 0))
+# If we don't have a massive # of samples (not sure what massive is yet), this code works
+#gap.after = do.call("c", lapply(table(subject), function(i) c(rep(2, i-1), (length(names(subject))+1))))
+#circos.par(gap.after = gap.after, cell.padding = c(0, 0, 0, 0))
 # else use this code
-#circos.par(gap.degree = 0.1, cell.padding = c(0, 0, 0, 0))
+circos.par(gap.degree = 0.1, cell.padding = c(0, 0, 0, 0))
 
 # Draw the samples and connect them
-chordDiagram(matches[, c(1, 3)], order = names(patient), grid.col = sample_color,
+chordDiagram(matches[, c(1, 3)], order = names(subject), grid.col = sample_color,
              directional = 1, annotationTrack = "grid", preAllocateTracks = list(
                list(track.height = 0.02))
 )
@@ -110,16 +116,17 @@ if (argv$showSampleLabels) {
   }, bg.border = NA)
 }
 
-# Add the patient track
-for(b in unique(patient)) {
-  model = names(patient[patient == b])
-  if (argv$showPatientLabels) {
-    highlight.sector(sector.index = model, track.index = 1, col = patient_color[b], 
+# Add the subject track
+for(b in unique(subject)) {
+  model = names(subject[subject == b])
+  if (argv$showSubjectLabels) {
+    highlight.sector(sector.index = model, track.index = 1, col = subject_color[b],
                      text = b, text.vjust = -1, niceFacing = TRUE)
   } else {
-    highlight.sector(sector.index = model, track.index = 1, col = patient_color[b], niceFacing = TRUE)
+    highlight.sector(sector.index = model, track.index = 1, col = subject_color[b], niceFacing = TRUE)
   }
 }
 
 circos.clear()
+print(paste("Output written to", argv$outputFile))
 print("Plot saved to Rplots.pdf")
