5cd28d343b1674968d59257438be4b3d0d27cbdd
Ryan Golhar <ryan.golhar@bms.com>
2019-01-10 20:11:49 +0000

    Run on on-demand instances

--- genotypeSamples.py
+++ genotypeSamples.py
@@ -97,7 +97,7 @@ def parseArguments(argv):
     #                    help="S3 Path to Targets BED File")
 
     job_args = parser.add_argument_group("AWS Batch Job Settings") 
-    job_args.add_argument('-q', '--job-queue', action="store", default="ngs-spot-job-queue",
+    job_args.add_argument('-q', '--job-queue', action="store", default="ngs-job-queue",
                           help="AWS Batch Job Queue")
     job_args.add_argument('-j', '--job-definition', action="store", default="freebayes:1",
                           help="AWS Batch Job Definition")
