77be92a780f215982c4b09bc329aae51c670dc63
Ryan Golhar <ryan.golhar@bms.com>
2019-01-18 11:35:50 -0500

    Fix circos output and update output file

--- analysisScripts/findSwaps.R
+++ analysisScripts/findSwaps.R
@@ -20,26 +20,31 @@ library(argparser, quietly=TRUE)
 p <- arg_parser("findSampleSwaps")
 p <- add_argument(p, "--meltedResults", help="Cohort-matcher melted results", default="meltedResults.txt")
 p <- add_argument(p, "--sampleToSubject", help="Sample to subject mapping", default="sampleToSubject.txt")
+p <- add_argument(p, "--outputFile", help="Output file to list swaps", default="swaps.txt")
+
+p <- add_argument(p, "--circosFile", help="Output file for circos plot", default="circos.pdf")
 p <- add_argument(p, "--showSampleLabels", help="Show sample labels in circos plot (Default: False)", flag=TRUE)
 p <- add_argument(p, "--showSubjectLabels", help="Show subject labels in circos plot (Default: False)", flag=TRUE)
-p <- add_argument(p, "--outputFile", help="Output file to list swaps", default="swaps.txt")
+
 argv <- parse_args(p)
+rm(p)
 ###
 
 # Read cohort matcher results
-print(paste("Reading", argv$meltedResults, sep=" "))
+loginfo(paste("Reading", argv$meltedResults, sep=" "))
 cm <- read.table(argv$meltedResults, header=TRUE, sep="\t", stringsAsFactors=FALSE)
-cm.same <- cm[ which(cm$Judgement=="SAME"), ]
+#cm.same <- cm[ which(cm$Judgement=="SAME"), ]
 
 # Read sample to Subject map
-print(paste("Reading", argv$sampleToSubject, sep=" "))
+loginfo(paste("Reading", argv$sampleToSubject, sep=" "))
 sample_to_subject <- read.table(argv$sampleToSubject, header=FALSE, sep="\t", stringsAsFactors=FALSE,
                                 na.strings = "")
 colnames(sample_to_subject) <- c("sample", "subject")
 
-print( paste("Read", length(unique(sample_to_subject$subject)), "subjects and",
+loginfo( paste("Read", length(unique(sample_to_subject$subject)), "subjects and",
              length(unique(sample_to_subject$sample)), "samples", sep=" ") )
 
+# Check that the sample set in cohort matcher results and sampleToSubject map match
 samples <- unique(c(cm$Sample1, cm$Sample2))
 if (setequal(samples, unique(sample_to_subject$sample)) == FALSE) {
   logerror("Samples in meltedResults and samplesToSubject map don't match")
@@ -55,44 +60,61 @@ rm(samples)
 #plotNumSNPsCompared(snpsCompared)
 #dev.off()
 
-print("Matching samples...")
-fileConn <- file(argv$outputFile)
+loginfo("Matching samples...")
 # For each sample in the list of samples, get the best match
 matches <- data.frame(from_sample=character(), from_subject=character(),
-                      to_sample=character(), to_subject=character(),
+                      to_sample=character(), to_subject=character(), swap=character(),
                       stringsAsFactors = FALSE)
 for (sample in sample_to_subject$sample) {
-  writeLines(sample)
+  writeLines(paste("Checking", sample, sep=" "))
   sample_matches <- cm[ which((cm$Sample1==sample | cm$Sample2==sample) & cm$Judgement=="SAME"), ]
   # if sample_matches is Empty, then sample doesn't match to another subject
   # for all sample_matches, make sure the sample matches to the same subject
+  writeLines(paste("  - Found", nrow(sample_matches), "match(es):", sep=" "))
   if (nrow(sample_matches) > 0) {
-    writeLines(paste("     Found", nrow(sample_matches), "match(s)", sep=" "))
     for (row_index in 1:nrow(sample_matches)) {
       if (sample_matches[row_index, "Sample1"] == sample) {
         matched_sample <- sample_matches[row_index, "Sample2"]
       } else {
         matched_sample <- sample_matches[row_index, "Sample1"]
       }
-      writeLines(paste("     Matched sample is", matched_sample, sep=" "))
+      writeLines(paste("    - ", matched_sample, sep=" "))
       # sample is the sample of interest
       # matched_sample is the matched sample
       # Now, make sure they are from the same subject
       subject1 <- sample_to_subject[sample_to_subject$sample==sample, 'subject']
       subject2 <- sample_to_subject[sample_to_subject$sample==matched_sample, 'subject']
-      if (subject1 != subject2) {
-        writeLines(paste("     Sample ", sample, " (USUBJID: ", subject1, ") matches to ", matched_sample,
+      if (subject1 == subject2) {
+        swap <- ""
+      } else {
+        writeLines(paste("    - Sample ", sample, " (USUBJID: ", subject1, ") matches to ", matched_sample,
                     " (USUBJID: ", subject2, ")", sep=""))
+        swap <- "x"
       }
-      matches <- rbind(matches, data.frame(from_sample=sample, from_subject=subject1, to_sample=matched_sample, to_subject=subject2))
+      matches <- rbind(matches, data.frame(from_sample=sample, from_subject=subject1, to_sample=matched_sample, to_subject=subject2, swap=swap))
     }
   }
 }
-close(fileConn)
 
-# Fix the data.frame to remove levels
+# Clean up
+rm(matched_sample, row_index, sample, subject1, subject2, swap)
+loginfo(paste("Writing results to", argv$outputFile, sep=" "))
+write.table(matches, argv$outputFile, quote=FALSE, sep="\t", row.names=FALSE)
+
+# Fix the data.frame to remove levels (for circlize)
 matches <- data.frame(lapply(matches, as.character), stringsAsFactors = FALSE)
 
+# matches contains duplicate rows of (from_sample, to_sample) and (to_sample, from_sample).   We need to remove these duplicates
+# before plotting, https://stackoverflow.com/questions/25297812/pair-wise-duplicate-removal-from-dataframe
+cols = c(1, 2, 3, 4)
+newdf = matches[,cols]
+for (i in 1:nrow(matches)){
+  newdf[i, ] = sort(matches[i,cols])
+}
+matches <- matches[!duplicated(newdf),]
+rm(cols, newdf)
+
+loginfo(paste("Saving circos plot to", argv$circosFile, sep=" "))
 # Construct the circos plot
 subject = c(structure(matches$from_subject, names=matches$from_sample), structure(matches$to_subject, names=matches$to_sample))
 subject = subject[!duplicated(names(subject))]
@@ -102,11 +124,17 @@ subject_color = structure(2:(length(unique(subject))+1), names=unique(subject))
 sample_color = structure(2:(length(names(subject))+1), names=names(subject))
 
 library(circlize)
-# If we don't have a massive # of samples (not sure what massive is yet), this code works
-#gap.after = do.call("c", lapply(table(subject), function(i) c(rep(2, i-1), (length(names(subject))+1))))
-#circos.par(gap.after = gap.after, cell.padding = c(0, 0, 0, 0))
-# else use this code
-circos.par(gap.degree = 0.1, cell.padding = c(0, 0, 0, 0))
+pdf(argv$circosFile)
+
+# Put a small gap between each subject
+if (length(subject) < 100) {
+  # If we don't have a massive # of samples (not sure what massive is yet), this code works
+  gap.after = do.call("c", lapply(table(subject), function(i) c(rep(2, i-1), (length(names(subject))+1))))
+  circos.par(gap.after = gap.after, cell.padding = c(0, 0, 0, 0))
+} else {
+  # else use this code
+  circos.par(gap.degree = 0.1, cell.padding = c(0, 0, 0, 0))
+}
 
 # Draw the samples and connect them
 chordDiagram(matches[, c(1, 3)], order = names(subject), grid.col = sample_color,
@@ -134,10 +162,8 @@ for(b in unique(subject)) {
     highlight.sector(sector.index = model, track.index = 1, col = subject_color[b], niceFacing = TRUE)
   }
 }
-
 circos.clear()
-print(paste("Output written to", argv$outputFile))
-print("Plot saved to Rplots.pdf")
+dev.off()
 
 # CanvasXpress
 #library(canvasXpress)
