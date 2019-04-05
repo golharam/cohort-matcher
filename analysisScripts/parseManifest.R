95e28472d655f5e297f26cc795da03c10adf5576
Ryan Golhar <ryan.golhar@bms.com>
2019-04-05 14:25:02 +0000

    Create bamsheet from manifst

--- analysisScripts/parseManifest.R
+++ analysisScripts/parseManifest.R
@@ -7,7 +7,7 @@
 ### 1.  Sample to Subject file
 
 ### Read arguments
-install.packages(c('argparser'))
+#install.packages(c('argparser'))
 library(argparser, quietly=TRUE)
 p <- arg_parser("cohort-matcher prereq")
 p <- add_argument(p, "--manifest", help="Manifest File", default="BMS-Manifest.csv")
@@ -21,8 +21,24 @@ sampleToSubjectFile <- argv$sampleToSubject
 ### Read manifest
 manifest <- read.csv(manifestFile)
 manifest <- manifest[manifest$VRUNIDSUFFIX==1,]
-sample_to_subject <- data.frame(paste(manifest$VENDORNAME, manifest$VRUNID, sep=""), manifest$USUBJID)
 
-### Write sample to subject file
+### Create the sample to subject file
+paste("Writing", sampleToSubjectFile)
+sample_to_subject <- data.frame(paste(manifest$VENDORNAME, manifest$VRUNID, sep=""), manifest$USUBJID)
 write.table(sample_to_subject, file=sampleToSubjectFile, sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
+### Create the RNA-Seq bamsheet
+paste("Writing", "rna_bamsheet.txt")
+rna_samples <- manifest[manifest$ASSAYMETHOD=="RNA-Seq", ]
+rna_samples$sample_name <- paste(rna_samples$VENDORNAME, rna_samples$VRUNID, sep="")
+rna_samples$bamfile <- paste("s3://bmsrd-ngs-results/", rna_samples$BMSPROJECTID, "/bam/",
+                             rna_samples$sample_name, ".GRCh37ERCC-ensembl75.decontaminated.genome.bam", sep="")
+rna_bamsheet <- data.frame(sample=rna_samples$sample_name, bamfile=rna_samples$bamfile, reference="GRCh37ERCC")
+write.table(rna_bamsheet, file="rna_bamsheet.txt", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+
+### Create the WES bamsheet
+# TODO: Sujaya, please fill in code here
+#wes_bamsheet <- data.frame(sample=..., bamfile=..., reference="hg19")
+
+# TODO: Merge rna_bamsheet + wes_bamsheet and write out master bamsheet
+paste("TODO: Merge rna_bamsheet + wes_bamsheet")
