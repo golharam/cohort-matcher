c7895462de88c0ed79d3f90bda2ff8681103d416
Ryan Golhar <ryan.golhar@bms.com>
2019-06-29 14:21:14 -0400

    Better version of circos with labels

--- analysisScripts/findSwaps.R
+++ analysisScripts/findSwaps.R
@@ -99,11 +99,13 @@ for (sample in sample_to_subject$sample) {
       if (subject1 == subject2) {
         swap <- ""
       } else {
-        writeLines(paste("    - Sample ", sample, " (USUBJID: ", subject1, ") matches to ", matched_sample,
-                    " (USUBJID: ", subject2, ")", sep=""))
+        writeLines(paste("    - Sample ", sample, " (USUBJID: ", subject1, ") matches to ", 
+                         matched_sample, " (USUBJID: ", subject2, ")", sep=""))
         swap <- "x"
       }
-      matches <- rbind(matches, data.frame(from_sample=sample, from_subject=subject1, to_sample=matched_sample, to_subject=subject2, swap=swap))
+      matches <- rbind(matches, data.frame(from_sample=sample, from_subject=subject1,
+                                           to_sample=matched_sample, to_subject=subject2,
+                                           swap=swap))
     }
   }
 }
@@ -117,8 +119,9 @@ write.table(matches, argv$outputFile, quote=FALSE, sep="\t", row.names=FALSE)
 # Fix the data.frame to remove levels (for circlize)
 matches <- data.frame(lapply(matches, as.character), stringsAsFactors = FALSE)
 
-# matches contains duplicate rows of (from_sample, to_sample) and (to_sample, from_sample).   We need to remove these duplicates
-# before plotting, https://stackoverflow.com/questions/25297812/pair-wise-duplicate-removal-from-dataframe
+# matches contain duplicate rows of (from_sample, to_sample) and (to_sample, from_sample).   
+# We need to remove these duplicates before plotting,
+# https://stackoverflow.com/questions/25297812/pair-wise-duplicate-removal-from-dataframe
 cols = c(1, 2, 3, 4)
 newdf = matches[,cols]
 for (i in 1:nrow(matches)){
@@ -127,22 +130,26 @@ for (i in 1:nrow(matches)){
 matches <- matches[!duplicated(newdf),]
 rm(cols, newdf)
 
+## Construct the circos plot
 loginfo(paste("Saving circos plot to", argv$circosFile, sep=" "))
-# Construct the circos plot
-subject = c(structure(matches$from_subject, names=matches$from_sample), structure(matches$to_subject, names=matches$to_sample))
-subject = subject[!duplicated(names(subject))]
-subject = subject[order(subject, names(subject))]
-
-subject_color = structure(2:(length(unique(subject))+1), names=unique(subject))
-sample_color = structure(2:(length(names(subject))+1), names=names(subject))
-
 library(circlize)
+
+subjects = c(structure(matches$from_subject, names=matches$from_sample),
+             structure(matches$to_subject, names=matches$to_sample))
+subjects = subjects[!duplicated(names(subjects))]
+subjects = subjects[order(subjects, names(subjects))]
+
+subject_color = structure(2:(length(unique(subjects))+1), names=unique(subjects))
+sample_color = structure(2:(length(names(subjects))+1), names=names(subjects))
+
 pdf(argv$circosFile)
 
-# Put a small gap between each subject
-if (length(subject) < 100) {
+# Set up the circos parameters:
+# 1. Put a small gap between each subject
+if (length(subjects) < 100) {
   # If we don't have a massive # of samples (not sure what massive is yet), this code works
-  gap.after = do.call("c", lapply(table(subject), function(i) c(rep(2, i-1), (length(names(subject))+1))))
+  gap.after = do.call("c", lapply(table(subjects), function(i) c(rep(2, i-1),
+                      (length(names(subjects))+1))))
   circos.par(gap.after = gap.after, cell.padding = c(0, 0, 0, 0))
 } else {
   # else use this code
@@ -150,29 +157,46 @@ if (length(subject) < 100) {
 }
 
 # Draw the samples and connect them
-chordDiagram(matches[, c(1, 3)], order = names(subject), grid.col = sample_color,
-             directional = 1, annotationTrack = "grid", preAllocateTracks = list(
-               list(track.height = 0.02))
+chordDiagram(matches[, c(1, 3)],
+             order = names(subjects),
+             grid.col = sample_color,
+             directional = 1,
+             annotationTrack = "grid",
+             preAllocateTracks = list(
+               list(track.height = 0.02)
+             )
+             #preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(matches)))))
 )
 
 # Add sample labels (only worth doing if we don't have lots of samples)
 if (argv$showSampleLabels) {
-  circos.track(track.index = 2, panel.fun = function(x, y) {
-    xlim = get.cell.meta.data("xlim")
-    ylim = get.cell.meta.data("ylim")
-    sector.index = get.cell.meta.data("sector.index")
-    circos.text(mean(xlim), mean(ylim), sector.index, col = "white", cex = 0.6, facing = "inside", niceFacing = TRUE)
-  }, bg.border = NA)
+  #circos.track(track.index = 2, panel.fun = function(x, y) {
+  #  xlim = get.cell.meta.data("xlim")
+  #  ylim = get.cell.meta.data("ylim")
+  #  sector.index = get.cell.meta.data("sector.index")
+  #  circos.text(mean(xlim), mean(ylim), sector.index, col = "white", cex = 0.6, 
+  #              facing = "inside", niceFacing = TRUE)
+  #}, bg.border = NA)
+
+  # Track index needs to be added here to move sample labels to the outside
+  circos.track(ylim = c(0, 1), 
+               panel.fun = function(x, y) {
+                 circos.text(CELL_META$xcenter, CELL_META$cell.ylim, CELL_META$sector.index,
+                             facing = "clockwise", niceFacing = TRUE, adj = c(1, 0.5), cex = 0.6)
+               },
+               track.height = 0.01, bg.border = NA)
 }
 
 # Add the subject track
-for(b in unique(subject)) {
-  model = names(subject[subject == b])
+for(b in unique(subjects)) {
+  model = names(subjects[subjects == b])
   if (argv$showSubjectLabels) {
     highlight.sector(sector.index = model, track.index = 1, col = subject_color[b],
-                     text = b, text.vjust = -1, niceFacing = TRUE)
+                     niceFacing = TRUE,
+                     facing = "bending.inside", text = b, text.vjust = -1)
   } else {
-    highlight.sector(sector.index = model, track.index = 1, col = subject_color[b], niceFacing = TRUE)
+    highlight.sector(sector.index = model, track.index = 1, col = subject_color[b],
+                     niceFacing = TRUE)
   }
 }
 circos.clear()
@@ -181,8 +205,8 @@ dev.off()
 # CanvasXpress
 if (argv$canvasXpress) {
   library(canvasXpress)
-  smpAnnot = as.data.frame(subject)
-  smpAnnot$sample = names(subject)
+  smpAnnot = as.data.frame(subjects)
+  smpAnnot$sample = names(subjects)
   connections = apply(as.matrix(cbind(matches[,1], matches[,3])), 1, as.list)
   cxData = data.frame(var1=rep.int(1, length(smpAnnot$sample)))
   rownames(cxData) = smpAnnot$sample
