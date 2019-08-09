fe2e226f1443d87b71ade01747a9a9432d1797ec
Ryan Golhar <ryan.golhar@bms.com>
2019-08-09 14:56:50 +0000

    Add comment

--- example/0-prepInputFiles.sh
+++ example/0-prepInputFiles.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-Rscript --vanilla  ~/NGS/cohort-matcher/analysisScripts/parseManifest.R --manifest BMS-Manifest-P-20170601-0007-CA209-358-EA-20181204-1.csv
+# TODO: Provide the manifest file for the project, since this is currently the
+# only place to get the sample to subject mapping
+
+MANIFEST=BMS-Manifest-P-20170601-0007-CA209-358-EA-20181204-1.csv
+
+Rscript --vanilla  ~/NGS/cohort-matcher/analysisScripts/parseManifest.R \
+  --manifest $MANIFEST
 
