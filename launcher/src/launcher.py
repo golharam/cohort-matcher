580d0b6ad1652628785eee4913cd9b0ac00c4c30
Ryan Golhar <ryan.golhar@bms.com>
2025-04-23 11:15:08 -0400

    Removing manifest parsing script, example script, in favor of consolidating to launcher

--- launcher/src/launcher.py
+++ launcher/src/launcher.py
@@ -177,6 +177,20 @@ def do_work(args, platform, project):
     # Read the samplesheet
     samples = read_samplesheet(args.sample_sheet)
 
+    '''
+    # 2. Genotype samples
+    source ~/workspace/NGS/cohort-matcher/env/bin/activate
+    ~/workspace/NGS/cohort-matcher/src/genotypeSamples.py -b bamsheet.txt -o s3://bmsrd-ngs-results/$PROJECTID/cohort-matcher
+
+    # 3. Compare sample genotypes (generate per-sample meltedResults)
+    ~/workspace/NGS/cohort-matcher/src/compareSamples.py -b bamsheet.txt -CD s3://bmsrd-ngs-results/$PROJECTID/cohort-matcher
+
+    # 4. Merge results
+    ~/workspace/NGS/cohort-matcher/src/mergeResults.py -b bamsheet.txt -CD s3://bmsrd-ngs-results/$PROJECTID/cohort-matcher
+
+    # 5. Find swaps
+    Rscript --vanilla ~/workspace/NGS/cohort-matcher/analysisScripts/findSwaps.R
+    '''
     # Copy reference workflow(s) to project
     workflows = copy_workflows(
         pipeline_config.config[args.platform], platform, project)
