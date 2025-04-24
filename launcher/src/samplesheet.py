613c7e59679fccec0df3de0317a5fb5ba27ede1f
Ryan Golhar <ryan.golhar@bms.com>
2025-04-23 22:38:05 -0400

    Fix how we read the samplesheet.txt

--- launcher/src/samplesheet.py
+++ launcher/src/samplesheet.py
@@ -1,11 +1,14 @@
 import csv
 
 def read_samplesheet(sample_sheet):
-    ''' Read the samplesheet '''
+    '''
+    Read the samplesheet consisting of a tab-separated file 
+    with a header with the following columns:
+    'subject_id', 'sample_id', 'bamfile', 'reference', 'assay'
+    '''
     with open(sample_sheet, 'r') as file_handle:
         reader = csv.DictReader(
-            file_handle, delimiter='\t',
-            fieldnames=['subject_id', 'sample_id', 'bamfile', 'reference']
+            file_handle, delimiter='\t'
         )
         samples = [row for row in reader]
     return samples
